258947f0fdfb1e500da88e94aaf69bd3487e3c20
Joel Mwaga <mwagahjoel14@gmail.com>
2025-04-13 22:53:42 +0300

    changed 1 file

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,8 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 import App from "../components/App";
+import React from "react";
 
 test("renders without errors", () => {
   expect(() => render(<App />)).not.toThrow();
@@ -20,3 +21,27 @@ test("renders the correct child components", () => {
   expect(container.querySelector("#home")).toBeInTheDocument();
   expect(container.querySelector("#about")).toBeInTheDocument();
 });
+
+function NavBar() {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
